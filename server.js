b6cddcfb98f630da27a19bc0cc84da95adc39f35
ThanushaW <thanushaw@outlook.com>
2023-07-10 04:44:47 +0530

    server change

--- server.js
+++ server.js
@@ -1,67 +1,80 @@
-var express = require('express');
-var path = require('path');
-var fs = require('fs');
-var MongoClient = require('mongodb').MongoClient;
-var bodyParser = require('body-parser');
-var app = express();
+let express = require('express');
+let path = require('path');
+let fs = require('fs');
+let MongoClient = require('mongodb').MongoClient;
+let bodyParser = require('body-parser');
+let app = express();
 
 app.use(bodyParser.urlencoded({
-    extended:true
+  extended: true
 }));
-
 app.use(bodyParser.json());
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,"index.html"));
+app.get('/profile-picture', function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
 });
 
-app.get('/get-profile',function(req,res){
-    var responce = res;
-    MongoClient.connect('mongodb://admin:password@localhost:27017',function(err,client){
-        if(err) throw err;
+// use when starting application locally
+let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 
-        var db = client.db('user-account');
-        var query = {userid:1};
-        db.collection('users').findOne(query,function(err,result){
-            if(err) throw err;
+// use when starting application as docker container
+let mongoUrlDocker = "mongodb://admin:password@mongodb";
 
-            client.close();
-            responce.send(result);
-        });
+// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
+// "user-account" in demo with docker. "my-db" in demo with docker-compose
+let databaseName = "my-db";
+
+app.post('/update-profile', function (req, res) {
+  let userObj = req.body;
+
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+    if (err) throw err;
+
+    let db = client.db(databaseName);
+    userObj['userid'] = 1;
+
+    let myquery = { userid: 1 };
+    let newvalues = { $set: userObj };
+
+    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+      if (err) throw err;
+      client.close();
     });
+
+  });
+  // Send response
+  res.send(userObj);
 });
 
-app.get('/update-profile',function(req,res){
-    var userObj=req.body;
-    var responce = res;
+app.get('/get-profile', function (req, res) {
+  let response = {};
+  // Connect to the db
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+    if (err) throw err;
 
-    MongoClient.connect('mongodb://admin:password@localhost:27017',function(err,client){
-        if(err) throw err;
+    let db = client.db(databaseName);
 
-        var db = client.db('user-account');
-        userObj['userid'] = 1;
-        var query = {userid:1};
-        var newValues = {$set:userObj};
-        db.collection('users').updateOne(query,newValues,{upsert:true},function(err,res){
-            if(err) throw err;
+    let myquery = { userid: 1 };
 
-            client.close();
-            responce.send(userObj);
-        });
+    db.collection("users").findOne(myquery, function (err, result) {
+      if (err) throw err;
+      response = result;
+      client.close();
 
+      // Send response
+      res.send(response ? response : {});
     });
+  });
 });
 
-
-
-
-app.get('/profile-picture',function(req,res){
-    var img = fs.readFileSync('profile-1.jpg');
-    res.writeHead(200,{'Content-Type':'image/jpg'});
-    res.end(img,'binary');
-});
-app.listen(3000,function(){
-   console.log("app listening on port 3000!");
+app.listen(3000, function () {
+  console.log("app listening on port 3000!");
 });
